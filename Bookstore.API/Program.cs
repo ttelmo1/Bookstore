01f624fde0acc486a55becf1b55f1b9b622edf4c
Telmo Bezerra <telmobsj@gmail.com>
2024-10-31 22:09:23 -0300

    feat: add new connection string to program

--- Bookstore.API/Program.cs
+++ Bookstore.API/Program.cs
@@ -5,7 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("BookstoreDb"));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
