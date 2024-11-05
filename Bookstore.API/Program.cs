699ffcfb501c0c5b32951d29035817326ca474cb
Telmo Bezerra <telmobsj@gmail.com>
2024-11-05 09:43:05 -0300

    feat: add application module and register services

--- Bookstore.API/Program.cs
+++ Bookstore.API/Program.cs
@@ -1,3 +1,4 @@
+using Bookstore.Application;
 using Bookstore.Infraestructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
+builder.Services.AddApplicationModule();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Bookstore.Application/ApplicationModule.cs
@@ -0,0 +1,25 @@
+﻿using Bookstore.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplicationModule(this IServiceCollection services)
+        {
+            services.AddServices();
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IBookService, BookService>();
+            return services;
+        }
+    }
+}
